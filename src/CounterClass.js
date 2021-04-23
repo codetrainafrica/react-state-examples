63813b35b6ba6097130636135f409c37f2d4800b
zak <codedpills@gmail.com>
2021-04-23 14:20:24 +0000

    implement state in class component

---
+++ src/CounterClass.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class CounterClass extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  handleIncreaseCount = (e) => {
+    e.preventDefault();
+    this.setState({count: this.state.count + 1})
+  }
+  handleDecreaseCount = (e) => {
+    e.preventDefault();
+    this.setState({count: this.state.count - 1})
+  }
+  render() {
+    return (
+      <div>
+        <h1>Counter: {this.state.count}</h1>
+        <hr/>
+        <button onClick={this.handleIncreaseCount}>Increase</button>
+        <button onClick={this.handleDecreaseCount}>Decrease</button>
+      </div>
+    );
+  }
+}
+
+export default CounterClass;
+
